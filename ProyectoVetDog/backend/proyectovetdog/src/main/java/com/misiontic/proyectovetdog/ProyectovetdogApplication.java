777e1d1c902461c6acfa35930302a484228f4f2e
Karen <aleka.toro@gmail.com>
2022-11-27 12:37:39 -0500

    version3

---
+++ ProyectoVetDog/backend/proyectovetdog/src/main/java/com/misiontic/proyectovetdog/ProyectovetdogApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.proyectovetdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectovetdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectovetdogApplication.class, args);
+	}
+
+}
---
+++ ProyectoVetDog/backend/proyectovetdog/src/test/java/com/misiontic/proyectovetdog/ProyectovetdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.proyectovetdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectovetdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
